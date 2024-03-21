b4f0e05980fbae69c5df55f5bb86e79903028000
Ammar Qammaz <ammarkov@ics.forth.gr>
2024-03-21 15:32:39 +0200

    ...

--- scripts/get_model.sh
+++ scripts/get_model.sh
@@ -5,8 +5,12 @@ cd "$DIR"
 
 cd ..
 
-wget http://ammar.gr/yolov3-tiny.weights
-wget http://ammar.gr/yolov7-tiny.weights
+wget http://ammar.gr/datasets/yolo/yolov3-tiny.weights
+wget http://ammar.gr/datasets/yolo/yolov4-tiny.weights
+wget http://ammar.gr/datasets/yolo/yolov7-tiny.weights
+
+wget http://ammar.gr/datasets/yolo/yolov4.weights
+wget http://ammar.gr/datasets/yolo/yolov7.weights
 
 
 exit 0
